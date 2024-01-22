aef716a6d043f21db97822818dc972fd4bd36532
paula <pauscalzo@gmail.com>
2024-01-22 11:47:41 -0300

    Primer Pre Entrega Paula Scalzo

--- src/ProductManager.js
+++ src/ProductManager.js
@@ -3,15 +3,14 @@ import fs from "fs"
 export class ProductManager {
     constructor () {
     this.products = [];
-    this.path = "src/products.json";
+    this.path = "./src/data/products.json";
     };
 
     getProducts(){
         try {
             const data = fs.readFileSync(this.path, "utf8");
             this.products = JSON.parse(data);
-            return this.products;
-                
+            return this.products;              
         }
         catch (error) {
             console.error("error al leer el archivo", error);
@@ -22,51 +21,49 @@ export class ProductManager {
     getProductById(pid){
         this.getProducts()
         const productId = this.products.find (product => product.id === pid);
-        console.log(productId);
-        console.log(pid);
+
         if (productId) {
             return productId;
         } else {
-            console.log("Not Found");
+            console.log("Producto no encontrado");
         }    
     };
 
-    addProduct(title, description, price, thumbnail, code, stock){
+    addProduct(product){
+        const { title, description, price, thumbnail, code, stock, category, status } = product;
 
-        if (title === "" || description === "" || price === "" || thumbnail === "" || code === "" || stock === "") {
-            console.log("Debe completar todos los campos.");
+        if (title === "" || description === "" || price === "" || thumbnail === "" || code === "" || stock === "" || category === "" || status === "") {
+            throw new Error("Debe completar todos los campos.");
         }
-
-        else if (this.products.some((prod) => prod.code === code)) {
-            console.log("El producto ya existe");
+    
+        if (this.products.some((prod) => prod.code === code)) {
+            throw new Error("El producto ya existe");
         }
-        else {
-
-            const newProduct = {
-                id: this.products.length + 1,
-                title,
-                description,
-                price,
-                thumbnail,
-                code,
-                stock
-            };
-
-            this.products.push(newProduct);
-            console.log("Producto agregado correctamente.");
-
-            try {
-
-                fs.writeFileSync(this.path, JSON.stringify(this.products));
-                console.log("producto guardado exitosamente");
-                
-            } catch (error) {
-
-                console.error("no se guardo el producto", error)
-                
-            }
+    
+        const newProduct = {
+            id: this.products.length + 1,
+            title: title,
+            description: description,
+            price: price,
+            thumbnail: thumbnail,
+            code: code,
+            stock: stock,
+            status: status,
+            category: category,
+        };
+    
+        this.products.push(newProduct);
+        console.log("Producto agregado correctamente.");
+    
+        try {
+            fs.writeFileSync(this.path, JSON.stringify(this.products));
+            console.log("producto guardado exitosamente");
+            return newProduct;
+        } catch (error) {
+            console.error("no se guardo el producto", error);
+            throw error;
         }
-    };
+    }
 
     updateProduct(id, productUpdate) {
         this.getProducts();
@@ -75,17 +72,14 @@ export class ProductManager {
             const Index = this.products.findIndex (product => product.id === id);
             this.products[Index] = {id, ...productUpdate};
             try {
-
                 fs.writeFileSync(this.path, JSON.stringify(this.products));
-                console.log("Archivo actualizado con éxito")
-                
+                console.log("Archivo actualizado con éxito")    
             } catch (error) {
                 console.error("no se pudo actualizar el archivo", error)
             }
         } else {
-            console.log("Not Found");
-        }
-        
+            console.log("No se encontro el producto");
+        }   
     };
 
     deleteProduct(id) {
@@ -95,17 +89,13 @@ export class ProductManager {
             const Index = this.products.findIndex (product => product.id === id);
             this.products.splice(Index, 1);
             try {
-
                 fs.writeFileSync(this.path, JSON.stringify(this.products));
-                console.log("El producto se ha borrado con éxito")
-                
+                console.log("El producto se ha borrado con éxito")  
             } catch (error) {
                 console.error("no se pudo borrar el producto", error)
             }
         } else {
-            console.log("Not Found");
-        }
-        
+            console.log("No se encontro el producto");
+        }  
     };
 };
-
--- src/app.js
+++ src/app.js
@@ -1,35 +1,22 @@
 import  express from 'express';
 import { ProductManager } from './ProductManager.js';
+import { CartManager } from './CartManager.js';
+import routerCarts from '../routes/carts.router.js';
+import routerProducts from '../routes/products.router.js';
 
 const app = express();
 const port = 8080;
 
-const productManager = new ProductManager();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+const pathProducts = './src/data/products.json';
+const pathCarts = './src/data/carts.json';
+
+export const productManager = new ProductManager(pathProducts);
+export const cartManager = new CartManager(pathCarts);
+
+app.use('/api/products', routerProducts);
+app.use('/api/carts', routerCarts);
 
 app.listen(port, () => console.log("servidor con express"))
-
-app.get('/products', async (req, res) => {
-    const limit = parseInt(req.query.limit)
-    const products = await productManager.getProducts()
-
-    if (!isNaN(limit) && limit > 0) {
-        const showProducts = products.slice(0, limit)
-        res.json(showProducts)
-    } else {
-        res.json(products)
-    }
-    
-})
-
-app.get('/products/:pid', async (req, res) => {
-    const pid = req.params.pid
-    const productId = await productManager.getProductById(pid)
-
-    if (!productId)  {
-        return res.json({error: "Producto no encontrado"}) 
-    } else {
-
-    res.json(productId)
-    }
-})
 
